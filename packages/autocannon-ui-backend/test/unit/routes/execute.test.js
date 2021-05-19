a69e70e48efc9aa29a530542188b8490089e9b12
Simone Busoli <simone.busoli@gmail.com>
2021-05-19 10:16:57 +0200

    chore: fix unit test

--- packages/autocannon-ui-backend/test/unit/routes/execute.test.js
+++ packages/autocannon-ui-backend/test/unit/routes/execute.test.js
@@ -5,7 +5,7 @@ const Fastify = require('fastify')
 
 t.test('execute', async t => {
   t.test('returns 404 if url is not valid', async t => {
-    const fastify = Fastify().register(require('../../'))
+    const fastify = Fastify().register(require('../../../'))
 
     const response = await fastify.inject({
       url: '/api/execute',
