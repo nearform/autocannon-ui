ab5d17d3227c1e063c23dc4a0d5c046528e3d4e7
gilach <grzegorz@ilach.pl>
2021-05-21 15:01:42 +0200

    fix(ui): disable react strict mode

--- packages/autocannon-ui-frontend/src/components/ResultSet.jsx
+++ packages/autocannon-ui-frontend/src/components/ResultSet.jsx
@@ -11,7 +11,8 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Tooltip
+  Tooltip,
+  AccordionDetails
 } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { makeStyles } from '@material-ui/core/styles'
@@ -57,108 +58,116 @@ export default function ResultSet({ data }) {
         >
           <Typography>{new Date(data.start).toLocaleString()}</Typography>
         </AccordionSummary>
-        <Grid container className={classes.cardContainer}>
-          <Grid item xs={12}>
-            <Typography variant="h6" className={classes.tableHeader}>
-              <label>Latency</label>
-              <Tooltip title="Histogram statistics about response latency.">
-                <HelpIcon color="primary" className={classes.helpIcon} />
-              </Tooltip>
-            </Typography>
+        <AccordionDetails>
+          <Grid container className={classes.cardContainer}>
+            <Grid item xs={12}>
+              <Typography variant="h6" className={classes.tableHeader}>
+                <label>Latency</label>
+                <Tooltip title="Histogram statistics about response latency.">
+                  <HelpIcon color="primary" className={classes.helpIcon} />
+                </Tooltip>
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TableContainer component={Paper}>
+                <Table className={classes.table}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Stat</TableCell>
+                      <TableCell>2.5%</TableCell>
+                      <TableCell>50%</TableCell>
+                      <TableCell>97.5%</TableCell>
+                      <TableCell>99%</TableCell>
+                      <TableCell>Avg</TableCell>
+                      <TableCell>Stddev</TableCell>
+                      <TableCell>Max</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className={classes.statHeader}>
+                        Latency
+                      </TableCell>
+                      <TableCell>{data.latency['p2_5']} ms</TableCell>
+                      <TableCell>{data.latency['p50']} ms</TableCell>
+                      <TableCell>{data.latency['p97_5']} ms</TableCell>
+                      <TableCell>{data.latency['p99']} ms</TableCell>
+                      <TableCell>{data.latency['average']} ms</TableCell>
+                      <TableCell>{data.latency['stddev']} ms</TableCell>
+                      <TableCell>{data.latency['max']} ms</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6" className={classes.tableHeader}>
+                <label>Volume</label>
+                <Tooltip title="Histogram statistics about the amount of requests that were sent per second and the response data throughput per second.">
+                  <HelpIcon color="primary" className={classes.helpIcon} />
+                </Tooltip>
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TableContainer component={Paper}>
+                <Table className={classes.table}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Stat</TableCell>
+                      <TableCell>2.5%</TableCell>
+                      <TableCell>50%</TableCell>
+                      <TableCell>97.5%</TableCell>
+                      <TableCell>99%</TableCell>
+                      <TableCell>Avg</TableCell>
+                      <TableCell>Stddev</TableCell>
+                      <TableCell>Max</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className={classes.statHeader}>
+                        Req/Sec
+                      </TableCell>
+                      <TableCell>{data.requests['p1']}</TableCell>
+                      <TableCell>{data.requests['p2_5']}</TableCell>
+                      <TableCell>{data.requests['p50']}</TableCell>
+                      <TableCell>{data.requests['p97_5']}</TableCell>
+                      <TableCell>{data.requests['average']}</TableCell>
+                      <TableCell>{data.requests['stddev']}</TableCell>
+                      <TableCell>{data.requests['min']}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className={classes.statHeader}>
+                        Bytes/Sec
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['p1'])}
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['p2_5'])}
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['p50'])}
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['p97_5'])}
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['average'])}
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['stddev'])}
+                      </TableCell>
+                      <TableCell>
+                        {prettyBytes(data.throughput['min'])}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TableContainer component={Paper}>
-              <Table className={classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Stat</TableCell>
-                    <TableCell>2.5%</TableCell>
-                    <TableCell>50%</TableCell>
-                    <TableCell>97.5%</TableCell>
-                    <TableCell>99%</TableCell>
-                    <TableCell>Avg</TableCell>
-                    <TableCell>Stddev</TableCell>
-                    <TableCell>Max</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className={classes.statHeader}>
-                      Latency
-                    </TableCell>
-                    <TableCell>{data.latency['p2_5']} ms</TableCell>
-                    <TableCell>{data.latency['p50']} ms</TableCell>
-                    <TableCell>{data.latency['p97_5']} ms</TableCell>
-                    <TableCell>{data.latency['p99']} ms</TableCell>
-                    <TableCell>{data.latency['average']} ms</TableCell>
-                    <TableCell>{data.latency['stddev']} ms</TableCell>
-                    <TableCell>{data.latency['max']} ms</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6" className={classes.tableHeader}>
-              <label>Volume</label>
-              <Tooltip title="Histogram statistics about the amount of requests that were sent per second and the response data throughput per second.">
-                <HelpIcon color="primary" className={classes.helpIcon} />
-              </Tooltip>
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TableContainer component={Paper}>
-              <Table className={classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Stat</TableCell>
-                    <TableCell>2.5%</TableCell>
-                    <TableCell>50%</TableCell>
-                    <TableCell>97.5%</TableCell>
-                    <TableCell>99%</TableCell>
-                    <TableCell>Avg</TableCell>
-                    <TableCell>Stddev</TableCell>
-                    <TableCell>Max</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className={classes.statHeader}>
-                      Req/Sec
-                    </TableCell>
-                    <TableCell>{data.requests['p1']}</TableCell>
-                    <TableCell>{data.requests['p2_5']}</TableCell>
-                    <TableCell>{data.requests['p50']}</TableCell>
-                    <TableCell>{data.requests['p97_5']}</TableCell>
-                    <TableCell>{data.requests['average']}</TableCell>
-                    <TableCell>{data.requests['stddev']}</TableCell>
-                    <TableCell>{data.requests['min']}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className={classes.statHeader}>
-                      Bytes/Sec
-                    </TableCell>
-                    <TableCell>{prettyBytes(data.throughput['p1'])}</TableCell>
-                    <TableCell>
-                      {prettyBytes(data.throughput['p2_5'])}
-                    </TableCell>
-                    <TableCell>{prettyBytes(data.throughput['p50'])}</TableCell>
-                    <TableCell>
-                      {prettyBytes(data.throughput['p97_5'])}
-                    </TableCell>
-                    <TableCell>
-                      {prettyBytes(data.throughput['average'])}
-                    </TableCell>
-                    <TableCell>
-                      {prettyBytes(data.throughput['stddev'])}
-                    </TableCell>
-                    <TableCell>{prettyBytes(data.throughput['min'])}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
-        </Grid>
+        </AccordionDetails>
       </Accordion>
     </React.Fragment>
   )
--- packages/autocannon-ui-frontend/src/main.jsx
+++ packages/autocannon-ui-frontend/src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './index.css'
-import App from './components/App'
+import App from './components/App.jsx'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
