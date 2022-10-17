7d3d03c3c3849b60e51c22a878510a6320a539f4
Laszlo Mile <mlacif1@gmail.com>
2022-10-17 11:51:54 +0200

    688: wait for document changes

--- packages/autocannon-ui-frontend/test/autocannon.test.js
+++ packages/autocannon-ui-frontend/test/autocannon.test.js
@@ -44,11 +44,19 @@ describe('Autocannon UI Puppeteer Test', function () {
   })
 
   it('should run the two tests and can compare them', async function () {
+    const navigationPromise = page.waitForNavigation({
+      waitUntil: 'domcontentloaded'
+    })
+
     await page.goto('http://localhost:3000/')
+    await navigationPromise
+
     await page.waitForSelector('div.MuiLinearProgress-bar', { hidden: true })
 
     await page.$eval('#url', el => (el.value = 'https://www.google.com'))
     await page.click('[data-testid="run-button"]')
+    await navigationPromise
+
     await page.waitForSelector('div.MuiLinearProgress-bar', { hidden: true })
 
     await page.evaluate(
@@ -60,6 +68,8 @@ describe('Autocannon UI Puppeteer Test', function () {
     await page.waitForSelector('#url')
     await page.$eval('#url', el => (el.value = 'https://www.yahoo.com'))
     await page.click('[data-testid="run-button"]')
+    await navigationPromise
+
     await page.waitForSelector('div.MuiLinearProgress-bar', { hidden: true })
 
     const resultsCheckboxes = await page.$$('[data-testid="result-checkbox"]')
@@ -67,8 +77,12 @@ describe('Autocannon UI Puppeteer Test', function () {
       await resultsCheckboxes[i].click()
     }
 
+    await navigationPromise
+
     await page.waitForSelector('[data-testid="compare-button"]')
     await page.click('[data-testid="compare-button"]')
+    await navigationPromise
+
     await page.waitForSelector('.compare-dialog')
   })
 })
