73504f6654453e1995ee2b91418e63fb591a1beb
Laszlo Mile <mlacif1@gmail.com>
2022-10-31 08:20:29 +0100

    716: ran prettier on appjs file

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-import path from "path";
+import path from 'path'
 
-import pino from "pino";
-import Fastify from "fastify";
-import * as autocannonUiBackend from "autocannon-ui-backend";
-import * as pkgDir from "pkg-dir";
-import fastifyStatic from "@fastify/static";
-import child_process from "child_process";
+import pino from 'pino'
+import Fastify from 'fastify'
+import * as autocannonUiBackend from 'autocannon-ui-backend'
+import * as pkgDir from 'pkg-dir'
+import fastifyStatic from '@fastify/static'
+import child_process from 'child_process'
 
 const transport = pino.transport({
-  target: "pino-pretty",
-  options: { colorize: true },
-});
+  target: 'pino-pretty',
+  options: { colorize: true }
+})
 
 const logger = pino(
   {
-    level: "info",
+    level: 'info',
     base: null,
-    timestamp: false,
+    timestamp: false
   },
   transport
-);
+)
 
 async function start() {
   try {
-    const address = await startServer();
-    logger.info(`Autocannon-UI started. Open ${address} in your browser.`);
+    const address = await startServer()
+    logger.info(`Autocannon-UI started. Open ${address} in your browser.`)
   } catch (e) {
-    logger.error(e);
+    logger.error(e)
   }
 }
 
 async function startServer() {
-  const fastify = Fastify();
-  fastify.register(autocannonUiBackend);
+  const fastify = Fastify()
+  fastify.register(autocannonUiBackend)
 
   const uiRoot = path.join(
     pkgDir.packageDirectorySync(),
-    "package/packages/autocannon-ui-frontend/dist"
-  );
+    'package/packages/autocannon-ui-frontend/dist'
+  )
 
   fastify.register(fastifyStatic, {
-    root: uiRoot,
-  });
+    root: uiRoot
+  })
 
-  const address = await fastify.listen();
-  await fastify.ready();
+  const address = await fastify.listen()
+  await fastify.ready()
 
   const start =
-    process.platform == "darwin"
-      ? "open"
-      : process.platform == "win32"
-      ? "start"
-      : "xdg-open";
+    process.platform == 'darwin'
+      ? 'open'
+      : process.platform == 'win32'
+      ? 'start'
+      : 'xdg-open'
 
-  child_process.exec(start + " " + address);
+  child_process.exec(start + ' ' + address)
 
-  return address;
+  return address
 }
 
-start();
+start()
