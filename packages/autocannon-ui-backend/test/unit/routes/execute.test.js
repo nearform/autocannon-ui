a0bc973061280d5b3cdddad68e5396f1a5f9a211
Simone Busoli <simone.busoli@nearform.com>
2023-03-02 13:40:24 +0100

    fix: support localhost urls (#802)

--- packages/autocannon-ui-backend/test/unit/routes/execute.test.js
+++ packages/autocannon-ui-backend/test/unit/routes/execute.test.js
@@ -4,7 +4,7 @@ import Fastify from 'fastify'
 import index from '../../../index.js'
 
 t.test('execute', async t => {
-  t.test('returns 404 if url is not valid', async t => {
+  t.test('returns 400 if url is not valid', async t => {
     const fastify = Fastify().register(index)
 
     const response = await fastify.inject({
@@ -16,5 +16,19 @@ t.test('execute', async t => {
     })
 
     t.same(response.statusCode, 400)
+  })
+
+  t.test('supports localhost urls with port', async t => {
+    const fastify = Fastify().register(index)
+
+    const response = await fastify.inject({
+      url: '/api/execute',
+      method: 'POST',
+      body: {
+        url: 'http://localhost:3000'
+      }
+    })
+
+    t.same(response.statusCode, 200)
   })
 })
