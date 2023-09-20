cf2729246c28a7e7aa1a3c8e62f3201b90bb658c
Nabeel Bukhari <nabeel.bukhari@hotmail.com>
2023-09-20 20:52:10 +0200

    Add port/host binding for cli

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import pino from 'pino'
 import Fastify from 'fastify'
 import fastifyStatic from '@fastify/static'
 import child_process from 'child_process'
+import { program } from 'commander'
 import { fileURLToPath } from 'url'
 import autocannonUiBackend from './packages/autocannon-ui-backend/index.js'
 
+program
+  .option("-p, --port <port>", "listening port")
+  .option("-h, --host <host>", "host to bind", "localhost")
+  .parse();
+
+const options = program.opts();
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -42,15 +49,20 @@ async function startServer() {
     root: uiRoot
   })
 
-  const address = await fastify.listen()
+  let address = ""
+  if (options.port || options.host) {
+    address = await fastify.listen({port: options.port, host: options.host})
+  } else {
+    address = await fastify.listen()
+  }
   await fastify.ready()
 
   const start =
     process.platform == 'darwin'
       ? 'open'
       : process.platform == 'win32'
-      ? 'start'
-      : 'xdg-open'
+        ? 'start'
+        : 'xdg-open'
 
   child_process.exec(start + ' ' + address)
 
