2d03abaf50646b4c2e213690024bbd894217834f
Nabeel Bukhari <nabeelbukhari@zalando.de>
2023-09-20 16:53:45 +0200

    Format Fix

--- packages/autocannon-ui-backend/test/unit/routes/compare.test.js
+++ packages/autocannon-ui-backend/test/unit/routes/compare.test.js
@@ -1,23 +1,26 @@
 import fs from 'fs'
-import path from 'path'
+
 import t from 'tap'
 import Fastify from 'fastify'
 
 import index from '../../../index.js'
 
 t.test('compare', async t => {
+  const a = JSON.parse(
+    fs.readFileSync('test/fixtures/fixture-a.json').toString()
+  )
+  const b = JSON.parse(
+    fs.readFileSync('test/fixtures/fixture-b.json').toString()
+  )
 
-  const a = JSON.parse(fs.readFileSync('test/fixtures/fixture-a.json').toString());
-  const b = JSON.parse(fs.readFileSync('test/fixtures/fixture-b.json').toString());
-  
   t.test('compare: returns 200 for comparing two valid results', async t => {
     const fastify = Fastify().register(index)
     const response = await fastify.inject({
       url: '/api/compare',
       method: 'POST',
-      body: { 
-        a, 
-        b 
+      body: {
+        a,
+        b
       }
     })
 
